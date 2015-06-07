d58372800ca8a3615bf92a30241dee9b49322412
TomWerner <tom-werner@uiowa.edu>
2015-06-07 17:48:47 -0500

    Initial commit of spring boot configuration

---
+++ src/test/java/judge/JudgeApplicationTests.java
@@ -0,0 +1,18 @@
+package judge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JudgeApplication.class)
+@WebAppConfiguration
+public class JudgeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
